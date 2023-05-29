d336b734b359fccbd108048e394baebaa280c795
KAIS JEMMALI <kais.jemmali@gmail.com>
2023-05-29 21:16:16 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Profile from "./profile/Profile";
 import photoProfile from "./photo.jpg";
 
 function App() {
+  const profile = {
+    fullName: "Kais Jemmali",
+    bio: "Hello, my name is Kais Jemmali, and I am learning Full Stack Developpement",
+    profession: "CEO",
+  };
   const handleName = (fullName) => {
     alert(`Hello, my name is ${fullName}`);
   };
   return (
     <div className="App">
       <Profile
-        fullName="Kais Jemmali"
-        bio="Hello, my name is Kais Jemmali, and I am learning Full Stack Developpement"
-        profession="CEO"
+        fullName={profile.fullName}
+        bio={profile.bio}
+        profession={profile.profession}
         handleName={handleName}
       >
-        {/* The children */}
         <img
           style={{ borderRadius: "100%", width: "200px", height: "200px" }}
           src={photoProfile}
